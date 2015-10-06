f43574b89b1e20798337a5c2072c449a62d95f5b
Richard McP <richard@cs.utexas.edu>
2015-10-06 13:16:53 -0400

    More info

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # emacs-pref
 
-cd
+sudo apt-get install emacs git mercurial
+
+# Main
+cd ~
 git clone https://github.com/rfmcpherson/emacs-pref.git .emacs.d
+
+# Evil-mode
+hg clone https://bitbucket.org/lyro/evil evil-mode
+
+# Lua-mode
 git clone https://github.com/immerrr/lua-mode.git
+
+# Multi-web-mode
+git clone https://github.com/fgallina/multi-web-mode.git
--- init.el
+++ init.el
@@ -43,15 +43,22 @@
 (windmove-default-keybindings)
 
 ; Multi-web-mode
-;(require 'multi-web-mode)
-;   (setq mweb-default-major-mode 'html-mode)
-;   (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-;                                            (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-;                      (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-;   (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-;   (multi-web-global-mode 1)
+(add-to-list 'load-path "~/.emacs.d/multi-web-mode")
+(require 'multi-web-mode)
+   (setq mweb-default-major-mode 'html-mode)
+   (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                                            (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                      (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+   (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+   (multi-web-global-mode 1)
 
 ; Evil-mode
-(add-to-list 'load-path "~/.emacs.d/evil")
+(add-to-list 'load-path "~/.emacs.d/evil-mode")
 (require 'evil)
 (evil-mode 0)
+
+ ;; Lua-mode.el
+(add-to-list 'load-path "~/.emacs.d/lua-mode")
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
