0fe884f7b7375a6682bf4a58545630c382b23773
richard <richardfmcpherson@gmail.com>
2016-07-26 17:29:06 -0400

    unfill-paragraph

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 
 ;; Kill *Completions* buffer on any command
 ;; http://stackoverflow.com/a/14995391/2479672
+;; TODO: switch hook to file load or something better
 (defun delete-completion-window-buffer (&optional output)
   (interactive)
   (dolist (win (window-list))
@@ -50,6 +51,20 @@
 (setq js-indent-level 2)
 ;; ===================================================================
 
+
+;; unfill-paragraph
+;; ===================================================================
+;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+(define-key global-map "\M-Q" 'unfill-paragraph)
+;; ===================================================================
 
 
 ;; Key bindings
